5529b6a5077fbd6b2848938b50cbae2e5335903f
Colt Bauman <cabauman@calmid.com>
2020-08-07 16:52:59 +0900

    Add demo code file

---
+++ MyFirstAnalyzer/MyFirstAnalyzer/Demo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new[] { 1 };
+
+            var a = list.Where(x => x > 1);
+
+            list
+                .Select(
+                    x =>
+                        list
+                            .Select(
+                                y =>
+                                    y + 1))
+                .Where(
+                    x =>
+                        x.First() > 1);
+
+            list
+                .Where(x =>
+            x > 1);
+
+            list
+                .Aggregate(
+                    0,
+                    (a, b) =>
+                        a + b);
+
+            list
+                .GroupBy(
+                    x =>
+                        x,
+                    x =>
+                        x);
+
+            list
+                .Where(
+                x =>
+                {
+                    return x > 1;
+                });
+        }
+
+        private static IEnumerable<int> TestMethod(int[] list)
+        {
+            return list.Where(x => x > 1);
+        }
+    }
+}
